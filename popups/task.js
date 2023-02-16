860fea66c2ca1a41ec678d0e5924f2a6575e8b15
Rovnin Nikita <Nikita.Rovnin@gmail.com>
2023-02-16 18:42:08 +0700

    Init commit

--- popups/task.js
+++ popups/task.js
@@ -1,21 +1,21 @@
 const modMain = document.getElementById('modal_main')
 const modSuccess = document.getElementById('modal_success')
-const closeBtns = document.getElementsByClassName('modal__close modal__close_times')
+const closeBtns = Array.from(document.getElementsByClassName('modal__close modal__close_times'))
 const doBtn = document.getElementsByClassName('show-success')[0]
 
+// Show window #modal_main
 modMain.classList.add('modal_active')
 
+// Show window #modal_seccess at click do button
 doBtn.onclick = () => {
     modSuccess.classList.add('modal_active')
 }
 
-let arr = Array.from(closeBtns)
-
-for (let i = 0; i < arr.length; i++) {
-    arr[i].onclick = () => {
-        for (const a of arr) {
-            a.parentElement.parentElement.classList.remove('modal_active')
+// Close all window at click close button
+for (let i = 0; i < closeBtns.length; i++) {
+    closeBtns[i].onclick = () => {
+        for (const i of closeBtns) {
+            i.parentElement.parentElement.classList.remove('modal_active')
         }
     }
 }
-
